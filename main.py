93f283c438c07a2e54f2ac047548a25141ab9d04
Matheus Hernandes <midia.matheus@gmail.com>
2018-08-13 14:53:11 -0300

    maybe working

--- main.py
+++ main.py
@@ -3,22 +3,33 @@ from transactions import Transaction
 import organizze
 import re
 
+def check_old_title(title, notes):
+    old_title = re.search(r"OLD_TITLE=(.*)", notes)
+    old_title = old_title.group() if old_title else False
+
+    if not old_title:
+        return None
+
+    return Transaction(title=old_title, new_title=title).save()
+
 def check(id, title, notes):
-    if "OLD=" in notes:
-        pass
+    if "OLD_TITLE=" in notes:
+        return check_old_title(title, notes)
 
     t = Transaction.objects(title=title)
 
-    if not len(t):
-        return Transaction(title=title, organizze_id=id, new_title=title).save()
+    if len(t) == 0:
+        return None
 
-    organizze.update_transaction(id, { "title": title })
+    new_title = t[0]["new_title"]
+    return organizze.update_transaction(id, { "title": new_title })
 
 def main():
     transactions = organizze.list_transactions()
 
     for t in transactions:
-        check(t["id"], t["description"])
+        check(t["id"], t["description"], t["notes"])
 
 if __name__ == "__main__":
     main()
+
--- transactions.py
+++ transactions.py
@@ -3,5 +3,4 @@ from mongoengine import *
 class Transaction(Document):
     title = StringField()
     new_title = StringField()
-    organizze_id = IntField()
 
