f046e4d66a2a6df508bd2b5665093d81af7040f5
Matheus Hernandes <midia.matheus@gmail.com>
2018-08-14 13:42:23 -0300

    fix order

--- main.py
+++ main.py
@@ -7,28 +7,31 @@ import os
 connect('organizze-title-indexer', port=int(os.environ.get("MONGODB_PORT", 27016)), host=os.environ.get("MONGODB_HOST", "127.0.0.1"))
 
 def check_old_title(title, notes):
+    if notes is None:
+        return None
+
     old_title = re.match(r"OLD_TITLE=(.*)", notes)
     old_title = old_title.group().replace("OLD_TITLE=", "") if old_title else False
 
-    if not old_title:
+    t = Transaction.objects(title=old_title).first()
+
+    if old_title is False or t:
         return None
 
+    print("Registering a new item...\n")
     return Transaction(title=old_title, new_title=title).save()
 
 def check(id, title, notes):
-    if notes is not None and "OLD_TITLE=" in notes:
-        print("Registering a new item...\n")
-        return check_old_title(title, notes)
-
     t = Transaction.objects(title=title).first()
 
-    if not t:
-        print("Item not found in db...\n")
-        return None
+    if t is None:
+        return check_old_title(title, notes)
 
-    new_title = t[0].new_title
-    print("Updating transaction...\n")
-    return organizze.update_transaction(id, { "title": new_title })
+    new_title = t["new_title"]
+    print("Updating transaction \"%s\" to \"%s\"...\n" % (title, new_title))
+    res = organizze.update_transaction(id, { "description": new_title })
+    print(res)
+    return res
 
 def main():
     transactions = organizze.list_transactions()
